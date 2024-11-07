37e1edcf554dd2e536fff5a17dc6f12ebe777b34
Alison Doyle <s00233102@atu.ie>
2024-11-07 12:09:57 +0000

    :sparkles: Can redirect user to website

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.qrcode;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    // Activity elements
+    private Button buttonScan;
+
+    private EditText etName;
+    private EditText etAddress;
+
+    // Properties
+    private IntentIntegrator _qrScan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +40,64 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Get activity elements
+        etName = findViewById(R.id.etName);
+        etAddress = findViewById(R.id.etAddress);
+        buttonScan = findViewById(R.id.buttonScan);
+
+        // Set up activity
+        _qrScan = new IntentIntegrator(this);
+
+        // Event listeners
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                _qrScan.initiateScan();
+            }
+        });
+
+        etAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String websiteUrl = etAddress.getText().toString();
+                RedirectUser(websiteUrl);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // On scan of qr code, capture result
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            // Check if the result contains data
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else {
+                try {
+                    // Convert contents from string to json so we can access individual fields
+                    JSONObject obj = new JSONObject(result.getContents());
+
+                    // Read in title and website url then populate edit text elements
+                    etName.setText(obj.getString("title"));
+                    etAddress.setText(obj.getString("website"));
+                } catch (JSONException e) {
+                    // Log error/stack trace
+                    e.printStackTrace();
+
+                    // Alert user something went wrong
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void RedirectUser(String websiteUrl) {
+//        Toast.makeText(this, "Website URL: " + websiteUrl, Toast.LENGTH_LONG).show();
+        Uri websiteUri = Uri.parse(websiteUrl);
+        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, websiteUri);
+        startActivity(websiteIntent);
     }
 }
